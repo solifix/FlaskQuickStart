1d9aabb97c7fe7dd07723a55b6bb10ba361f9cd4
Prijma Sofia <romaniv.sofiia@vpu29lviv.ukr2.education>
2024-05-30 12:18:33 +0300

    base

--- app.py
+++ app.py
@@ -1,34 +1,32 @@
-from flask import Flask, url_for, render_template
-from markupsafe import escape
+from flask import render_template, Flask
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return render_template('home.html')
+@app.route("/users")
+def user_list():
+    users = [
+        'Sovyak',
+        'Gul',
+        'Sovyak'
+    ]
+
+    return render_template(
+        'user_list.html',
+        users=users
+    )
 
 
-@app.route("/bye")
-def bye_world():
-    return "<p>Bye, World!</p>"
+@app.route("/users/<string:username>")
+def user(username):
+    return render_template(
+        'user.html',
+        username=username,
+    )
 
 
-@app.route("/posts")
-def posts():
-    return render_template('posts.html', posts=range(1, 11))
 
 
-@app.route('/posts/<int:post_id>')
-def show_post(post_id):
-    return f'Post {post_id}'
 
 
-@app.route("/hello/<username>")
-def hello_user(username):
-    return render_template('hello_user.html', username=username)
 
-
-@app.route('/login')
-def login():
-    return 'login'
