06d7ade75af4f72f9a224d8ed7dfb5db6691bb83
Prijma Sofia <romaniv.sofiia@vpu29lviv.ukr2.education>
2024-06-13 14:16:01 +0300

    css

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-from flask import render_template, request, redirect, url_for, session
-from flask import Flask
+from flask import render_template, Flask, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
 
-user_database = {
-    "sofia": "221133",
-    "solomia": "123456",
-    "eva": "654321",
-
+users = {
+    "vasyl": '123456',
+    "sergiy": '123123',
+    "vlad": "qwerty"
 }
-
-app.secret_key = 'jhu5436hgvgbf'
 
 
 @app.route("/")
@@ -18,18 +14,27 @@ def home():
     return render_template('home.html')
 
 
+@app.route("/posts")
+def posts():
+    return render_template('posts.html', posts=range(1, 11))
+
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    return render_template('show_posts.html', post_id=post_id)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+    if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
 
-        if username in user_database and user_database[username] == password:
+        if username in users and users[username] == password:
             session['username'] = username
+            flash(f'Вітаємо,{username}! ви успішно авторизувалися!')
 
-            return redirect(
-                url_for('user', username=request.form['username'])
-            )
+            return redirect(url_for('hello_user', username=username))
 
     return render_template('login.html')
 
@@ -39,29 +44,35 @@ def register():
     return render_template('register.html')
 
 
-@app.route("/users/<username>")
-def user(username):
-    if 'username' in session and session['username'] == username:
-        return render_template(
-            'user.html',
-            username=username
-        )
+app.secret_key = '123123'
+
+
+@app.route("/user/<username>")
+def hello_user(username):
+    return render_template('hello_user.html', username=username)
 
 
 @app.route('/users')
-def users():
+def show_users_profile():
     users = [
         'kyznetsov',
         'arsen',
         'Sovyak'
     ]
-    return render_template('users.html', users=users)
+    return render_template('user_list.html', users=users)
 
 
-@app.route("/about")
+@app.route('/about')
 def about():
     return render_template('about.html')
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+
+    return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
